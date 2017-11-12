586c36871882c9ff6ed0002cefb94b65bc41e576
AMakarov <amakarov@inttrust.ru>
2017-11-12 18:16:54 +0300

    Init

---
+++ Thermostation_client.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Thermostation_client.ino
+ Created:	12.11.2017 18:14:02
+ Author:	allex
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
